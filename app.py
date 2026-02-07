2ef2b9a47c1f44d0ee7cb6d88b9c223bbeeb782a
Nicolas Faine <nicolasfainepaz@gmail.com>
2026-02-07 16:31:23 +0100

    Copilots code

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from analysis.password import password
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    score = None
+    score_str = None
+    feedback = None
+    password_input = ""
+    
+    if request.method == "POST":
+        password_input = request.form.get("password", "")
+        score, score_str, feedback = password(password_input)
+    
+    return render_template("index.html", score=score, score_str=score_str, feedback=feedback, password_input=password_input)
 
 if __name__ == "__main__":
     app.run(debug=True)
